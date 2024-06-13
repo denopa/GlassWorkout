91bd732e454bd75b62681362b6b0221ca1d15bdd
denopa <patrick.de-nonneville@m4x.org>
2024-06-13 16:49:50 +0100

    Initial Commit

---
+++ GlassWorkout Watch App/GlassWorkoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlassWorkoutApp.swift
+//  GlassWorkout Watch App
+//
+//  Created by Patrick de Nonneville on 13/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct GlassWorkout_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
